69450370328d45b70452734809c2b386aad62b40
chong <cshao@cs.unc.edu>
2016-04-15 15:27:30 +0800

    add zoom in feature to the video .

--- Grab_MultipleCameras/Grab_MultipleCameras.cpp
+++ Grab_MultipleCameras/Grab_MultipleCameras.cpp
@@ -49,7 +49,53 @@ static const uint32_t c_countOfImagesToGrab = 10;
 // Application Notes (AW000649xx000)
 // provide more information about this topic.
 // The bandwidth used by a FireWire camera device can be limited by adjusting the packet size.
+
+// TODO: implement a zoom in here.
 static const size_t c_maxCamerasToUse = 2;
+
+void function(int event, int x, int y, int flags, void* param) {
+
+//     switch (event) {
+//          case CV_EVENT_LBUTTONDOWN:
+//              if (flags & CV_EVENT_FLAG_CTRLKEY) {
+ //                 printf("Left button clicked in corrdinates %d, %d with CTRL\n", x, y);
+ //             }
+ //             else { 
+ //                 printf("Left button clicked in coordinates %d, %d\n", x, y);  
+ //             }
+ //             break;
+ //    }
+     if  ( event == EVENT_LBUTTONDOWN )
+     {     
+          if (*(double*)param < (double)50) { 
+              *(double*)param = *(double*)param + 0.1;
+          } 
+          cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+          cout << "param: " << *(double*)param << endl;
+     }
+     else if  ( event == EVENT_RBUTTONDOWN )
+     {
+          if (*(double*)param >= (double)1) { 
+              *(double*)param = *(double*)param - 0.1;
+          }
+          cout << "Right button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+          cout << "param: " << *(double*)param << endl;
+     }
+     else if  ( event == EVENT_MBUTTONDOWN )
+     {
+          cout << "Middle button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+     }
+     else if (event == EVENT_MOUSEHWHEEL) {
+          cout << "mouse wheel scrolled - position (" << x << ", " << y << ")" << endl;
+     }
+     else if ( event == EVENT_MOUSEMOVE )
+     {
+          cout << "Mouse move over the window - position (" << x << ", " << y << ")" << endl;
+     }
+
+}
+
+
 
 int main(int argc, char* argv[])
 {
@@ -96,8 +142,13 @@ int main(int argc, char* argv[])
         CImageFormatConverter fc;
 				fc.OutputPixelFormat = PixelType_RGB8packed;
 				CPylonImage image;
-	      Mat cv_img(3840, 2748, CV_8UC3);
-				
+	Mat cv_img(3840, 2748, CV_8UC3);
+        int curr_x_lim = 3840; 
+        int curr_y_lim = 2748;
+	Mat cv_img2(curr_x_lim, curr_y_lim, CV_8UC3);
+        double parameter = 1.0;	
+        double scale = 1; 
+        		
         // Grab c_countOfImagesToGrab from the cameras.
         //for( int i = 0; i < c_countOfImagesToGrab && cameras.IsGrabbing(); ++i)
         while (cameras.IsGrabbing()) {
@@ -116,33 +167,38 @@ int main(int argc, char* argv[])
 #endif
 
             // Print the index and the model name of the camera.
-            cout << "Camera " <<  cameraContextValue << ": " << cameras[ cameraContextValue ].GetDeviceInfo().GetModelName() << endl;
+     //       cout << "Camera " <<  cameraContextValue << ": " << cameras[ cameraContextValue ].GetDeviceInfo().GetModelName() << endl;
 
             // Now, the image data can be processed.
-            cout << "GrabSucceeded: " << ptrGrabResult->GrabSucceeded() << endl;
-            cout << "SizeX: " << ptrGrabResult->GetWidth() << endl;
-            cout << "SizeY: " << ptrGrabResult->GetHeight() << endl;
+     //       cout << "GrabSucceeded: " << ptrGrabResult->GrabSucceeded() << endl;
+     //       cout << "SizeX: " << ptrGrabResult->GetWidth() << endl;
+     //       cout << "SizeY: " << ptrGrabResult->GetHeight() << endl;
             const uint8_t *pImageBuffer = (uint8_t *) ptrGrabResult->GetBuffer();
-            cout << "Gray value of first pixel: " << (uint32_t) pImageBuffer[0] << endl << endl;
+     //       cout << "Gray value of first pixel: " << (uint32_t) pImageBuffer[0] << endl << endl;
             
-						if (ptrGrabResult->GrabSucceeded()) {
-							  fc.Convert(image, ptrGrabResult);
-								cv_img = cv::Mat(ptrGrabResult->GetHeight(),  ptrGrabResult->GetWidth(), CV_8UC3,(uint8_t*)image.GetBuffer());
-								Size size(500, 358);
-								Mat dst;
-								resize(cv_img, dst, size);
-								if (cameraContextValue == 0) {
-									imshow("CV_Image0", dst);		
-								}  
-								else {
-									imshow("CV_Image1", dst);		
-								}
-								waitKey(1);
-                if(waitKey(30)==27){
+            if (ptrGrabResult->GrabSucceeded()) {
+		fc.Convert(image, ptrGrabResult);
+		cv_img = cv::Mat(ptrGrabResult->GetHeight(),  ptrGrabResult->GetWidth(), CV_8UC3,(uint8_t*)image.GetBuffer());
+                curr_x_lim = (int)((double)curr_x_lim / parameter); 
+                curr_y_lim = (int)((double)curr_y_lim / parameter);
+                parameter = 1;
+                cv_img2 = cv_img(Rect(0, 0, curr_x_lim, curr_y_lim));
+                Size size(500, 358);
+		Mat dst;
+		resize(cv_img2, dst, size);
+	        if (cameraContextValue == 0) {
+		    imshow("CV_Image0", dst);	
+                    cvSetMouseCallback("CV_Image0", function, &parameter);	
+		}  
+		else {
+		    imshow("CV_Image1", dst);		
+		}                
+		waitKey(1);
+                if(waitKey(30)==27) {
                       cameras.StopGrabbing();
                 }
-						}
-		    }
+	   }
+       }
     }
     catch (const GenericException &e)
     {

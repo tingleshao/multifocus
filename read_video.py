0da26b4e2298e6b8f351ac6e681cb6d5ae4701f7
chong <cshao@cs.unc.edu>
2016-05-05 17:15:06 +0800

    add frame number in the video.

--- read_video.py
+++ read_video.py
@@ -6,9 +6,11 @@ cap1 = cv2.VideoCapture('/home/chong/Data/acA3800-14uc__21833709__20160422_14161
 
 frame_counter = 0 
 
-# TODO play two videos 
-# TODO attach them 
+# TODO stitich them 
 # TODO measurement: framerate, bandwidth utilization, etc. 
+# TODO: go some deep shit: tree data sturcture\
+# TODO: go some deep shit: H.265 multi scale support investigate in x265.
+
 
 while True:
   #  while(cap.isOpened()):
@@ -16,7 +18,10 @@ while True:
     frame0 = cv2.resize(frame0, (500, 378))
     ret, frame1 = cap1.read()
     frame1 = cv2.resize(frame1, (500, 378))
-    print frame_counter
+    m,n = frame1.shape[:2]
+    cv2.putText(frame0, "frame: " + str(frame_counter), (5, m-5),  cv2.FONT_HERSHEY_PLAIN, fontScale=1.0, color=(255,255,255), thickness=1)
+    cv2.putText(frame1, "frame: " + str(frame_counter), (5, m-5),  cv2.FONT_HERSHEY_PLAIN, fontScale=1.0, color=(255,255,255), thickness=1)
+    #print frame_counter
     frame_counter += 1
   # because the video recording is terminated due to file size issue, the last frame has a problem, we cannot reach it otherwise it will break
     if frame_counter == cap0.get(cv2.CAP_PROP_FRAME_COUNT)-1 or frame_counter == cap1.get(cv2.CAP_PROP_FRAME_COUNT) -1:
@@ -25,17 +30,13 @@ while True:
         cap1.set(cv2.CAP_PROP_POS_FRAMES, 0)
     gray0 = cv2.cvtColor(frame0, cv2.COLOR_BGR2GRAY)
     gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY) 
-     #   rows,cols = gray.shape
-
-     #   gray = cv2.resize(gray, (rows/3, cols/3)) 
     cv2.imshow('frame0', gray0)
     cv2.imshow('frame1', gray1) 
-    #cap = cv2.VideoCapture('/home/chong/Data/acA3800-14uc__21833705__20160422_141738868.avi')
-    #cap.release()
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 
 cap0.release()
+cap1.release()
 cv2.destroyAllWindows()
 

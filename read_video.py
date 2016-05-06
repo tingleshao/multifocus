ce7ec36c996d84bd3dd6642dfc4b98517f90be48
chong <cshao@cs.unc.edu>
2016-05-06 17:05:19 +0800

    made the stitching work.

--- read_video.py
+++ read_video.py
@@ -13,20 +13,18 @@ class Stitcher:
         (kpsA, featuresA) = self.detectAndDescribe(imageA)
         (kpsB, featuresB) = self.detectAndDescribe(imageB) 
   
-    M = self.matchKeyPoints(kpsA, kpsB, featuresA, featuresB, ratio, reporjThresh) 
+        M = self.matchKeypoints(kpsA, kpsB, featuresA, featuresB, ratio, reprojThresh) 
     
-    if M is None:
-        return None
+        if M is None:
+            return None
 
-    (matches, H, status) = M 
-    result = cv2.warpPerspective(imageA, H, (imageA.shape[1] + imageB.shape[1], imageA.shape[0]))
-    result[0:imageB.shape[0], 0:imageB.shape[1]] = imageBf
-
-    if showMatches:
-        vis = self.drawMatches(imageA, imageB, kpsA, kpsB, matches, status) 
-
-        return (result, vis) 
-    return result 
+        (matches, H, status) = M 
+        result = cv2.warpPerspective(imageA, H, (imageA.shape[1] + imageB.shape[1], imageA.shape[0]))
+        result[0:imageB.shape[0], 0:imageB.shape[1]] = imageB
+        if showMatches:
+            vis = self.drawMatches(imageA, imageB, kpsA, kpsB, matches, status) 
+            return (result, vis) 
+        return result 
  
 
     def detectAndDescribe(self, image):
@@ -49,19 +47,15 @@ class Stitcher:
         matcher = cv2.DescriptorMatcher_create("BruteForce")
         rawMatches = matcher.knnMatch(featuresA, featuresB, 2)
         matches = [] 
-    
         for m in rawMatches:
             if len(m) == 2 and m[0].distance < m[1].distance * ratio:
                 matches.append((m[0].trainIdx, m[0].queryIdx)) 
-
-
         if len(matches) > 4: 
             ptsA = np.float32([kpsA[i] for (_, i) in matches])
             ptsB = np.float32([kpsB[i] for (i, _) in matches]) 
     
             (H, status) = cv2.findHomography(ptsA, ptsB, cv2.RANSAC, reprojThresh) 
             return (matches, H, status) 
-    
         return None 
   
      
@@ -75,7 +69,7 @@ class Stitcher:
         for ((trainIdx, queryIdx), s) in zip(matches, status): 
             if s == 1:
                 ptA = (int(kpsA[queryIdx][0]), int(kpsA[queryIdx][1]))
-                ptB = (int(kpsB[trainIdx][0] + wA, int(kpsB[trainIdx][1]))
+                ptB = (int(kpsB[trainIdx][0]) + wA, int(kpsB[trainIdx][1]))
                 cv2.line(vis, ptA, ptB, (0, 255, 0), 1)
    
         return vis 
@@ -86,21 +80,42 @@ cap1 = cv2.VideoCapture('/home/chong/Data/acA3800-14uc__21833709__20160422_14161
 
 frame_counter = 0 
 
-# TODO stitich them 
+first_frame0 = None
+first_frame1 = None
+
 # TODO measurement: framerate, bandwidth utilization, etc. 
 # TODO: go some deep shit: tree data sturcture\
 # TODO: go some deep shit: H.265 multi scale support investigate in x265.s
 
+stitched = False
+
 while True:
   #  while(cap.isOpened()):
     ret, frame0 = cap0.read()
-    frame0 = cv2.resize(frame0, (500, 378))
+    frame0 = cv2.resize(frame0, (500, 357))
+    if (first_frame1 == None) and (frame_counter == 0):
+        first_frame1 = frame0
     ret, frame1 = cap1.read()
-    frame1 = cv2.resize(frame1, (500, 378))
+    frame1 = cv2.resize(frame1, (500, 357))
+    if (first_frame0 == None) and (frame_counter == 0): 
+        first_frame0 = frame1 
+
+    if not stitched:
+        stitched = True
+        first_frame0 = imutils.resize(first_frame0, width=500)
+        first_frame1 = imutils.resize(first_frame1, width=500)
+        stitcher = Stitcher()
+        (result, vis) = stitcher.stitch([first_frame0, first_frame1], showMatches=True)
+      
+        cv2.imshow("image A", first_frame0)
+        cv2.imshow("image B", first_frame1) 
+        cv2.imshow("Keypoint Matches", vis) 
+        cv2.imshow("Result", result)      
+  
+
     m,n = frame1.shape[:2]
     cv2.putText(frame0, "frame: " + str(frame_counter), (5, m-5),  cv2.FONT_HERSHEY_PLAIN, fontScale=1.0, color=(255,255,255), thickness=1)
     cv2.putText(frame1, "frame: " + str(frame_counter), (5, m-5),  cv2.FONT_HERSHEY_PLAIN, fontScale=1.0, color=(255,255,255), thickness=1)
-    #print frame_counter
     frame_counter += 1
   # because the video recording is terminated due to file size issue, the last frame has a problem, we cannot reach it otherwise it will break
     if frame_counter == cap0.get(cv2.CAP_PROP_FRAME_COUNT)-1 or frame_counter == cap1.get(cv2.CAP_PROP_FRAME_COUNT) -1:
